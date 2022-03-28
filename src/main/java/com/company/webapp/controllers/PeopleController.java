2a5d8b1e875a74c9b35ed1eedd5d77402b1e4631
Vitaliy Babich <v.babich.225@gmail.com>
2022-03-29 00:48:09 +0300

    add create function

--- src/main/java/com/company/webapp/controllers/PeopleController.java
+++ src/main/java/com/company/webapp/controllers/PeopleController.java
@@ -1,12 +1,11 @@
 package com.company.webapp.controllers;
 
 import com.company.webapp.dao.PersonDAO;
+import com.company.webapp.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -25,10 +24,25 @@ public class PeopleController {
         model.addAttribute("people", personDAO.index());
         return "people/index";
     }
+
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model){
         // Получаем одного человека по его ID из DAO и передадим на отображение в представление
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model){
+        //создание нового человека
+        model.addAttribute("person", new Person());
+
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person){
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
